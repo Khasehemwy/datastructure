e53100462fbdd00cd593ec915528781cd67b9769
Isaac Chen <tingyi364@gmail.com>
2020-12-04 23:42:47 +0800

    enable hidpi scaling
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
-
+#include "Class.h"
 #include <QApplication>
+
+Citys cities;
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
